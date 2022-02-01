35531ac14e40e050237f69e43ddb0b91b048d42f
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-02-01 12:21:26 +0100

    Completed Task-1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,16 +14,16 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            emp_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
             todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            emp_name = emp_res.get('username')
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
                     file.write(
                         '"{}","{}","{}","{}"\n'.format(
                             id,
-                            emp_name,
+                            user_name,
                             todo.get('completed'),
                             todo.get('title')
                         )
